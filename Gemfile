2d3eb695b5214d4898df54cddfec4f6c5238938d
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-03-25 14:30:36 +0000

    Blank Rails 3.2.17 app
    rails new content-store --skip-active-record --skip-sprockets
    --skip-javascript --skip-test-unit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.17'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+
+
+
+
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContentStore::Application.config.secret_token = '17d18dc5ad73cad8c4dac0eb0e151ae8232b38ab9a388bd8078dc02a2966e8198be71ef0e08dc3d8a4eec81c72c3ae64f5d41dd3ecdefca9c30ad3c51a1c35b1'
