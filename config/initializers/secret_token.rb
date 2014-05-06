2c04bbb7c6e0c20fc6f0f7a11eae8ac5c4b828de
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-05-06 11:01:14 +0000

    Remove the secret_token setting.
    Leaving this file in place so that if (when?) this is upgraded to Rails4
    we can find this comment, and add the necessary setting.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-ContentStore::Application.config.secret_token = '17d18dc5ad73cad8c4dac0eb0e151ae8232b38ab9a388bd8078dc02a2966e8198be71ef0e08dc3d8a4eec81c72c3ae64f5d41dd3ecdefca9c30ad3c51a1c35b1'
+# ContentStore::Application.config.secret_token = '17d18dc5ad73cad8c4dac0eb0e151ae8232b38ab9a388bd8078dc02a2966e8198be71ef0e08dc3d8a4eec81c72c3ae64f5d41dd3ecdefca9c30ad3c51a1c35b1'
