92ecdfca629536efeaed0cc8396de41bb983a9e5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-05-06 10:50:32 +0000

    Switch to using rails-api as the base for this.
    This is Rails with the bits that aren't necessart for api-only apps
    removed.  See https://github.com/rails-api/rails-api#readme for more
    details.
    
    This is the result of merging the output of `rails-api new content-store
    --skip-active-record --skip-sprockets --skip-javascript
    --skip-test-unit`
    with all the changes we'd made to the app so far.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,18 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
 ContentStore::Application.config.secret_token = '17d18dc5ad73cad8c4dac0eb0e151ae8232b38ab9a388bd8078dc02a2966e8198be71ef0e08dc3d8a4eec81c72c3ae64f5d41dd3ecdefca9c30ad3c51a1c35b1'
