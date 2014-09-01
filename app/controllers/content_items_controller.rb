92bb406188cfbb9b08e58ed188a02a91b42b5eec
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-09-01 16:07:08 +0000

    Integrate with url-arbiter

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,5 +1,8 @@
+require 'govuk/client/url_arbiter'
+
 class ContentItemsController < ApplicationController
   before_filter :parse_json_request, :only => [:update]
+  before_filter :register_with_url_arbiter, :only => [:update]
 
   def show
     item = ContentItem.find_by(:base_path => params[:base_path])
@@ -23,5 +26,17 @@ class ContentItemsController < ApplicationController
     @request_data = JSON.parse(request.body.read).except('base_path')
   rescue JSON::ParserError
     head :bad_request
+  end
+
+  def register_with_url_arbiter
+    Rails.application.url_arbiter_api.reserve_path(params["base_path"], "publishing_app" => @request_data["publishing_app"])
+  rescue GOVUK::Client::Errors::Conflict => e
+    item = ContentItem.new(@request_data.merge("base_path" => params[:base_path]))
+    if e.response["errors"] and e.response["errors"]["base"]
+      item.errors.set(:base_path, e.response["errors"]["base"])
+    else
+      item.errors.add(:base_path, "url-arbiter rejected registration: #{e.response.raw_body}")
+    end
+    render :json => item, :status => :conflict
   end
 end
