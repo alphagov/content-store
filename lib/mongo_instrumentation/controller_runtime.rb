322e9fe8de23904447529791165a3078dbb59d4e
Tekin Suleyman <tekin@tekin.co.uk>
2014-11-03 15:02:28 +0000

    Add MongoDB instrumentation to action logging
    We can generate more fine-grained instrumentation of MongoDB queries directly
    in the request logs by subscribing to the notifications generated by moped (the
    connection adapter used by mongoid). For example:
    
      Completed 200 OK in 3ms (Views: 1.2ms | Mongo: 0.7ms)

---
+++ lib/mongo_instrumentation/controller_runtime.rb
@@ -0,0 +1,28 @@
+module MongoInstrumentation
+  # Used to extend ActionController to output additional logging information on
+  # the duration of Mongo queries.
+  module ControllerRuntime
+    extend ActiveSupport::Concern
+
+    protected
+
+    def append_info_to_payload(payload)
+      super
+      payload[:mongo_runtime] = MongoInstrumentation::MopedSubscriber.runtime || 0
+      MongoInstrumentation::MopedSubscriber.reset_runtime
+    end
+
+    module ClassMethods
+      def log_process_action(payload)
+        super.tap do |messages|
+          runtime = payload[:mongo_runtime]
+          messages << ("Mongo: %.1fms" % runtime.to_f)
+        end
+      end
+    end
+  end
+end
+
+ActiveSupport.on_load(:action_controller) do
+  include MongoInstrumentation::ControllerRuntime
+end
---
+++ lib/mongo_instrumentation/runtime_registry.rb
@@ -0,0 +1,14 @@
+require 'active_support/per_thread_registry'
+
+module MongoInstrumentation
+  # This is a namespaced thread locals registry for tracking the duration of
+  # mongo queries.
+  #
+  # See the documentation of ActiveSupport::PerThreadRegistry for further
+  # details.
+  class RuntimeRegistry
+    extend ActiveSupport::PerThreadRegistry
+
+    attr_accessor :mongo_runtime
+  end
+end
