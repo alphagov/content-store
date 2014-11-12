6d42c21e7a2aae27ee55919a56d69e83552ced3e
Pedro Moreira <pedro.moreira@unboxedconsulting.com>
2014-11-12 15:33:03 +0000

    Set explicit path for cron in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 require 'whenever'
 
+env :PATH, '/usr/local/bin:/usr/bin:/bin'
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
