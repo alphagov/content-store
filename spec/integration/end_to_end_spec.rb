729fb1f8c4eebfca5aadca90e29918c610053029
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-11-27 17:57:03 +0000

    Add integration test for locale linking behaviours
    I refactored some of the specs to use some more shared context setup, and to
    reduce the length of some of the examples which appeared. I found the previous
    examples quite long and each example was actually testing several things. Since
    I wanted to add yet another detail to a test scenario, I preferred to break it
    up to simplify them and have each example cover only one particular aspect of
    behaviour.

--- spec/integration/end_to_end_spec.rb
+++ spec/integration/end_to_end_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "End-to-end behaviour", :type => :request do
 
   let(:data) {{
+    "locale" => "en",
     "base_path" => "/vat-rates",
     "content_id" => SecureRandom.uuid,
     "title" => "VAT rates",
@@ -33,55 +34,103 @@ describe "End-to-end behaviour", :type => :request do
   end
 
   describe "linking items" do
-    let(:linked_data_1) { attributes_for(:content_item, :with_content_id).stringify_keys }
-    let(:linked_data_2) { attributes_for(:content_item, :with_content_id).stringify_keys }
-    let(:linked_data_3) { attributes_for(:content_item, :with_content_id).stringify_keys }
-    let(:data_with_links) do
-      data.merge({
-        "links" => {
-          "related" => [linked_data_1["content_id"], linked_data_3["content_id"]],
-          "connected" => [linked_data_2["content_id"], linked_data_1["content_id"]],
-        },
-      })
-    end
+    let(:linked_data_1) { attributes_for(:content_item, :with_content_id, locale: "en").stringify_keys }
+    let(:linked_data_2) { attributes_for(:content_item, :with_content_id, locale: "en").stringify_keys }
 
-    it "should return details of linked items, ignoring missing ones" do
-      create_item(linked_data_1)
-      create_item(linked_data_2)
-
-      create_item(data_with_links)
-
+    subject(:links) {
       get "/content/vat-rates"
       expect(response.status).to eq(200)
       links = JSON.parse(response.body)["links"]
+    }
 
-      expect(links.keys).to match_array(["connected", "related"])
+    context "linked item which already existed" do
+      before(:each) {
+        create_item(linked_data_1)
+        create_item(data.merge(
+          "links" => {
+            "related" => [linked_data_1["content_id"]],
+            "connected" => []
+          }
+        ))
+      }
 
-      related_paths = links["related"].map {|i| i["base_path"] }
-      expect(related_paths).to eq([linked_data_1["base_path"]]) # ignored linked_3 entry
+      it "should include all like hash keys even if empty" do
+        expect(links.keys).to match_array(["connected", "related"])
+      end
 
-      connected_paths = links["connected"].map {|i| i["base_path"] }
-      expect(connected_paths).to eq([linked_data_2["base_path"], linked_data_1["base_path"]])
+      it "should return details of linked items" do
+        related_paths = links["related"].map {|i| i["base_path"] }
+        expect(related_paths).to eq([linked_data_1["base_path"]])
+      end
+
+      it "should include the locale of the linked item" do
+        expect(links["related"].map { |i| i["locale"] }).to eq([linked_data_1["locale"]])
+      end
     end
 
-    it "should include details of items that were added after the original item" do
-      create_item(linked_data_1)
+    context "linked item which does not exist" do
+      before(:each) {
+        create_item(linked_data_1)
+        create_item(data.merge(
+          "links" => {
+            "related" => [linked_data_1["content_id"], linked_data_2["content_id"]]
+          }
+        ))
+      }
 
-      create_item(data_with_links)
+      it "should ignore the missing one" do
+        related_paths = links["related"].map {|i| i["base_path"] }
+        expect(related_paths).not_to include(linked_data_2["base_path"])
+        expect(related_paths).to include(linked_data_1["base_path"])
+      end
+    end
 
-      create_item(linked_data_3)
+    context "linked item added after the original item" do
+      before(:each) {
+        create_item(data.merge(
+          "links" => {
+            "related" => [linked_data_1["content_id"]]
+          }
+        ))
+        create_item(linked_data_1)
+      }
 
-      get "/content/vat-rates"
-      expect(response.status).to eq(200)
-      links = JSON.parse(response.body)["links"]
+      it "should include details of items" do
+        related_paths = links["related"].map {|i| i["base_path"] }
+        expect(related_paths).to eq([linked_data_1["base_path"]])
+      end
+    end
 
-      expect(links.keys).to match_array(["connected", "related"])
+    context "translations of linked items exist" do
+      let(:linked_data_1_fr) {
+        attributes_for(:content_item,
+          content_id: linked_data_1["content_id"],
+          locale: "fr",
+          base_path: linked_data_1["base_path"] + ".fr"
+        ).stringify_keys
+      }
 
-      related_paths = links["related"].map {|i| i["base_path"] }
-      expect(related_paths).to eq([linked_data_1["base_path"], linked_data_3["base_path"]])
+      before(:each) {
+        create_item(linked_data_1_fr)
+        create_item(linked_data_1)
+        create_item(linked_data_2)
+        create_item(data.merge(
+          "locale" => "fr",
+          "links" => {
+            "related" => [linked_data_1["content_id"], linked_data_2["content_id"]]
+          }
+        ))
+      }
 
-      connected_paths = links["connected"].map {|i| i["base_path"] }
-      expect(connected_paths).to eq([linked_data_1["base_path"]])
+      it "should link by preference to the item with matching locale" do
+        expect(links["related"][0]["base_path"]).to eq(linked_data_1_fr["base_path"])
+        expect(links["related"][0]["locale"]).to eq(linked_data_1_fr["locale"])
+      end
+
+      it "should fall back on the english version if no item found with matching locale" do
+        expect(links["related"][1]["base_path"]).to eq(linked_data_2["base_path"])
+        expect(links["related"][1]["locale"]).to eq(linked_data_2["locale"])
+      end
     end
   end
 end
