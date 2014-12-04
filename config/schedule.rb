2677a861655efd66cd7a7b34032284c7a9dc0f82
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-12-04 15:06:29 +0000

    Run cleanup task every night from cron
    This also cleans up the cron config to be consistent with other apps.
    In particular it changes the output to go to cron.log and
    cron.error.log.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,31 +1,15 @@
 require 'whenever'
 
+# default cron env is "/usr/bin:/bin" which is not sufficient as govuk_setenv is in /usr/local/bin
 env :PATH, '/usr/local/bin:/usr/bin:/bin'
 
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-set :output, "log/heartbeat_log.log"
-job_type :rake, "cd :path && govuk_setenv content-store bundle exec rake :task --silent :output"
+set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
+job_type :rake, "cd :path && govuk_setenv content-store bundle exec rake :task :output"
 
 every 1.minute do
   rake "heartbeat_messages:send"
+end
+
+every 1.day, :at => "2:00 am" do
+  rake "housekeeping:cleanup_publish_intents"
 end
