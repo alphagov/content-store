c20925766186b06fdd5c0a0ba2365d25d5d64219
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-02-11 11:45:49 +0000

    Require a base_path in the routes.
    Using parens around a route parameter makes that param optional.  This
    meant that requests for eg /content were being routed through to
    content_items#show, which errored because it expected base_path to be
    populated.

--- spec/routing/publish_intent_routing_spec.rb
+++ spec/routing/publish_intent_routing_spec.rb
@@ -13,6 +13,10 @@ describe "routing of publish_intent requests", :type => :routing do
     it "should not match a base_path without a leading /" do
       expect(:get => "/publish-intentfoo").not_to be_routable
     end
+
+    it "should require a base_path" do
+      expect(:get => "/publish-intent").not_to be_routable
+    end
   end
 
   context "PUT route" do
@@ -27,6 +31,10 @@ describe "routing of publish_intent requests", :type => :routing do
     it "should not match a base_path without a leading /" do
       expect(:put => "/publish-intentfoo").not_to be_routable
     end
+
+    it "should require a base_path" do
+      expect(:put => "/publish-intent").not_to be_routable
+    end
   end
 
   context "DELETE route" do
@@ -40,6 +48,10 @@ describe "routing of publish_intent requests", :type => :routing do
 
     it "should not match a base_path without a leading /" do
       expect(:delete => "/publish-intentfoo").not_to be_routable
+    end
+
+    it "should require a base_path" do
+      expect(:delete => "/publish-intent").not_to be_routable
     end
   end
 end
