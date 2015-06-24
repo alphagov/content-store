3419a02d6a2e195b38b5d620f9c69c3ad5a9c3f4
Tekin Suleyman <tekin@tekin.co.uk>
2015-06-24 16:39:29 +0100

    Increase test coverage
    Just to make sure that the mere presence of an authentication header is enough
    to authorise the request

--- spec/integration/access_limited_spec.rb
+++ spec/integration/access_limited_spec.rb
@@ -29,4 +29,16 @@ describe "Fetching an access-limited content item", :type => :request do
       expect(data['title']).to eq(access_limited_content_item.title)
     end
   end
+
+  context "request with an unauthorised user id specified in the header" do
+    it "returns an 403 (Forbidden) response" do
+      get "content/#{access_limited_content_item.base_path}",
+        {}, { 'X-Govuk-Authenticated-User' => 'unauthorised-user' }
+
+      expect(response.status).to eq(403)
+      data = JSON.parse(response.body)
+
+      expect(response.body).to eq("{}")
+    end
+  end
 end
