e86ecf596c22d1603b5d4f37d624f762b6ecf11c
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-09-14 15:29:19 +0000

    Remove circularity from fetching_content_item_spec
    As part of the refactoring in #130, this integration test was updated to
    use the Presenter to generate its expectation. This meant that it
    wasn't testing the full stack, and was only testing that the returned
    JSON matched what the presenter generated. There was nowhere in the test
    suite that actually asserted against the specifics of what the
    content-store should return in its JSON response.
    
    This updates it to make assertions about the specifics of the JSON
    returned.

--- spec/integration/fetching_content_item_spec.rb
+++ spec/integration/fetching_content_item_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe "Fetching content items", :type => :request do
-  let(:content_item) { create(:content_item) }
 
   context "an existing content item" do
+    let(:content_item) {
+      create(:content_item,
+        :base_path => "/vat-rates",
+        :content_id => SecureRandom.uuid,
+        :title => "VAT rates",
+        :description => "Current VAT rates",
+        :format => "answer",
+        :need_ids => ["100136"],
+        :public_updated_at => 30.minutes.ago,
+        :details => {"body" => "<div class=\"highlight-answer\">\n<p>The standard <abbr title=\"Value Added Tax\">VAT</abbr> rate is <em>20%</em></p>\n</div>\n"},
+      )
+    }
     before(:each) { get_content content_item }
 
     it "returns a 200 OK response" do
@@ -12,7 +23,44 @@ describe "Fetching content items", :type => :request do
 
     it "returns the presented content item as JSON data" do
       expect(response.content_type).to eq("application/json")
-      expect(response.body).to eq(present(content_item))
+      data = JSON.parse(response.body)
+
+      expect(data.keys).to match_array(%w[
+        base_path
+        title
+        description
+        format
+        need_ids
+        locale
+        analytics_identifier
+        phase
+        public_updated_at
+        updated_at
+        details
+        links
+      ])
+
+      expect(data).to include(
+        "base_path" => "/vat-rates",
+        "title" => "VAT rates",
+        "description" => "Current VAT rates",
+        "format" => "answer",
+        "need_ids" => ["100136"],
+        "locale" => "en",
+        "analytics_identifier" => nil,
+        "phase" => nil,
+      )
+      expect(data["details"]).to eq({
+        "body" => "<div class=\"highlight-answer\">\n<p>The standard <abbr title=\"Value Added Tax\">VAT</abbr> rate is <em>20%</em></p>\n</div>\n",
+      })
+    end
+
+    it "outputs the timestamp fields correctly" do
+      content_item.reload # reload to pick up any time rounding in the database.
+
+      data = JSON.parse(response.body)
+      expect(data["public_updated_at"]).to eq(content_item.public_updated_at.as_json)
+      expect(Time.parse(data["updated_at"])).to be_within(1.second).of(Time.now.utc)
     end
 
     it "sets cache headers to expire in the default TTL" do
@@ -66,6 +114,28 @@ describe "Fetching content items", :type => :request do
 
     it "returns a 200 OK response" do
       expect(response.status).to eq(200)
+    end
+
+    it "includes the correct data in the expanded representation of the linked item" do
+      data = JSON.parse(response.body)
+
+      linked_item_data = data["links"]["related"].first
+      expect(linked_item_data.keys).to match_array(%w[
+        base_path
+        title
+        description
+        locale
+        api_url
+        web_url
+      ])
+
+      expect(linked_item_data).to include(
+        "base_path" => linked_item.base_path,
+        "title" => linked_item.title,
+        "description" => linked_item.description,
+        "locale" => linked_item.locale,
+        "web_url" => Plek.new.website_root + linked_item.base_path,
+      )
     end
 
     it "corrrectly expands linked items with internal API URLs" do
