14bef8898cdbc31c2b7de2aa2e0fe3f681f4b267
Tijmen Brommet <tijmen@gmail.com>
2015-10-15 12:03:29 +0100

    Add redirects for school commissioner URLs
    These URLs redirect to a new spelling of the organisation, much like
    the RAIB case (#154). In this case we can't find redirects in
    router-data, so we assume they have been added via another way.
    
    We need to make them redirects without content_id because the
    content_id are duplicated.
    
    Trello: https://trello.com/c/cme0kKzQ

---
+++ db/migrate/20151015103826_fix_redirects_for_schools_commissioner.rb
@@ -0,0 +1,20 @@
+class FixRedirectsForSchoolsCommissioner < Mongoid::Migration
+  def self.up
+    existing_redirects = CSV.read("#{Rails.root}/db/migrate/20151015103826_fix_redirects_for_schools_commissioner.csv")
+
+    existing_redirects.each do |from_base_path, to_base_path|
+      content_item = ContentItem.find_by(base_path: from_base_path)
+      content_item.update_attributes!(
+        content_id: nil,
+        format: "redirect",
+        publishing_app: "whitehall",
+        rendering_app: nil,
+        redirects: [{ path: from_base_path, type: 'exact', destination: to_base_path }],
+        routes: [],
+      )
+    end
+  end
+
+  def self.down
+  end
+end
