882bbfd77e60450da97701821db21aae95e93d0a
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2015-11-23 17:18:43 +0000

    Generate HMRC Manuals content_ids from base path...
    HMRC manuals API generates v5 uuids from a sha1 hash of the base path.
    This change ensures the content store employs the same method when
    generating content ids.
    See
    https://github.com/alphagov/hmrc-manuals-api/commit/6316ebc205e0ccdac3c00ece88f710a179c5fae5
    for details.

--- db/migrate/20151119100304_update_blank_content_item_content_ids.rb
+++ db/migrate/20151119100304_update_blank_content_item_content_ids.rb
@@ -1,18 +1,26 @@
+require "uuidtools"
+
 class UpdateBlankContentItemContentIds < Mongoid::Migration
   def self.up
     content_items = []
 
+    ContentItem.where(content_id: nil, publishing_app: "hmrc-manuals-api").each do |item|
+      item.set(content_id: UUIDTools::UUID.sha1_create(UUIDTools::UUID_URL_NAMESPACE, item.base_path))
+      content_items << "#{item.content_id},#{item.publishing_app},#{item.base_path}"
+    end
+
+    report_path = "./tmp/generated_hmrc_manuals_api_content_ids.txt"
+    report_missing_content_ids(report_path, content_items)
+
+    content_items = []
+
     ContentItem.where(content_id: nil).each do |item|
       item.set(content_id: SecureRandom.uuid)
-
       content_items << "#{item.content_id},#{item.publishing_app},#{item.base_path}"
     end
 
     report_path = "./tmp/generated_content_ids.txt"
-    File.write(report_path, "content_id,publishing_app,base_path")
-    File.write(report_path, content_items.join("\n"))
-    puts "#{content_items.size} missing content_id values assigned."
-    puts "Details written to #{report_path}"
+    report_missing_content_ids(report_path, content_items)
 
     content_items = []
 
@@ -22,12 +30,24 @@ class UpdateBlankContentItemContentIds < Mongoid::Migration
     end
 
     report_path = "./tmp/assigned_public_updated_at.txt"
-    File.write(report_path, "public_updated_at,publishing_app,base_path")
-    File.write(report_path, content_items.join("\n"))
+    report_to_file(report_path, "public_updated_at,publishing_app,base_path", content_items)
     puts "#{content_items.size} missing public_updated_at values assigned."
     puts "Details written to #{report_path}"
   end
 
   def self.down
+  end
+
+private
+
+  def self.report_missing_content_ids(path, items)
+    report_to_file(path, "content_id,publishing_app,base_path", items)
+    puts "#{items.size} missing content_id values assigned."
+    puts "Details written to #{path}"
+  end
+
+  def self.report_to_file(path, headings, items)
+    File.write(path, headings)
+    File.write(path, items.join("\n"))
   end
 end
