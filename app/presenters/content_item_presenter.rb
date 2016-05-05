666148f5a1b4959122b01bd33e180cd4c0cb7c4f
Tijmen Brommet <tijmen@gmail.com>
2016-05-05 10:51:02 +0100

    Sort PUBLIC_ATTRIBUTES
    This makes it easier to find things.

--- app/presenters/content_item_presenter.rb
+++ app/presenters/content_item_presenter.rb
@@ -7,20 +7,20 @@ class ContentItemPresenter
   RESOLVER = ContentTypeResolver.new("text/html")
 
   PUBLIC_ATTRIBUTES = %w(
+    analytics_identifier
     base_path
     content_id
-    title
-    format
-    schema_name
     document_type
-    need_ids
-    locale
-    updated_at
-    first_published_at
-    public_updated_at
-    phase
-    analytics_identifier
     expanded_links
+    first_published_at
+    format
+    locale
+    need_ids
+    phase
+    public_updated_at
+    schema_name
+    title
+    updated_at
   ).freeze
 
   def initialize(item, api_url_method)
