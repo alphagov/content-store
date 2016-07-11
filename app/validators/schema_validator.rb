9cc86d4d1742ac79a74ce18e4083ec56cfcb6a6e
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-07-11 15:21:39 +0000

    Add schema validator

---
+++ app/validators/schema_validator.rb
@@ -0,0 +1,74 @@
+require "json-schema"
+
+class SchemaValidator
+  def initialize(type:, schema_name: nil, schema: nil)
+    @type = type
+    @schema = schema
+    @schema_name = schema_name
+  end
+
+  def validate(payload)
+    @payload = payload
+
+    return true if schema_name_exception?
+
+    errors = JSON::Validator.fully_validate(
+      schema,
+      payload,
+      errors_as_objects: true,
+    )
+
+    return true if errors.empty?
+
+    errors = Hash[errors.map.with_index { |e, i| [i, present_error(e)] }]
+
+    Airbrake.notify_or_ignore(
+      {
+        error_class: "SchemaValidationError",
+        error_message: "Error validating payload against schema"
+      },
+      parameters: {
+        errors: errors,
+        message_data: payload
+      }
+    )
+    false
+  end
+
+private
+
+  attr_reader :payload, :type
+
+  def present_error(error_hash)
+    # The schema key just contains an addressable, which is not informative as
+    # the schema in use should be clear from the error class and message
+    error_hash = error_hash.reject { |k, _| k == :schema }
+
+    if error_hash.has_key?(:errors)
+      error_hash[:errors] = Hash[
+        error_hash[:errors].map do |k, errors|
+          [k, Hash[errors.map.with_index { |e, i| [i, present_error(e)] }]]
+        end
+      ]
+    end
+
+    error_hash
+  end
+
+  def schema
+    @schema || JSON.load(File.read("../govuk-content-schemas/dist/formats/#{schema_name}/frontend/#{type}.json"))
+  rescue Errno::ENOENT => error
+    Airbrake.notify_or_ignore(error, parameters: {
+      explanation: "#{payload} is missing schema_name #{schema_name} or type #{type}"
+    })
+    {}
+  end
+
+  def schema_name
+    @schema_name || payload["schema_name"] || payload["format"]
+  end
+
+  def schema_name_exception?
+    schema_name.to_s.match(/placeholder_/)
+  end
+end
