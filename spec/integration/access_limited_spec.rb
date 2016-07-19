01bcfcb0eb185a3b67226a9a738bfb3599ef65b7
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-07-19 14:32:33 +0000

    Add contextual hash braces rubocop rule
    Mongoid::Document#any_of doesn't always behave as expected
    unless explict hash braces are used for each of the clauses.
    Rubocop config needs an override to allow this.
    I've amended a spec to conform to the contextual braces setting.

--- spec/integration/access_limited_spec.rb
+++ spec/integration/access_limited_spec.rb
@@ -19,7 +19,7 @@ describe "Fetching an access-limited content item", type: :request do
   context "request with an authorised user ID specified in the header" do
     before do
       get "/content/#{access_limited_content_item.base_path}",
-        {}, 'X-Govuk-Authenticated-User' => authorised_user_uid
+        {}, { 'X-Govuk-Authenticated-User' => authorised_user_uid }
     end
 
     it "returns the details for the requested item" do
@@ -38,7 +38,7 @@ describe "Fetching an access-limited content item", type: :request do
   context "request with an unauthorised user ID specified in the header" do
     it "returns a 403 (Forbidden) response" do
       get "/content/#{access_limited_content_item.base_path}",
-        {}, 'X-Govuk-Authenticated-User' => 'unauthorised-user'
+        {}, { 'X-Govuk-Authenticated-User' => 'unauthorised-user' }
 
       json = JSON.parse(response.body)
 
