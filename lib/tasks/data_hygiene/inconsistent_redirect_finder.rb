69efc07ae6ffbe16cbc5cf5df67f212b8c977fcb
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-09-23 15:53:52 +0000

    gds-api-adapters now raises NotFound on a 404.

--- lib/tasks/data_hygiene/inconsistent_redirect_finder.rb
+++ lib/tasks/data_hygiene/inconsistent_redirect_finder.rb
@@ -9,7 +9,11 @@ class InconsistentRedirectFinder
     content_items.select do |content_item|
       next if content_item.format == "redirect"
 
-      route = Rails.application.router_api.get_route(content_item.base_path)
+      begin
+        route = Rails.application.router_api.get_route(content_item.base_path)
+      rescue GdsApi::HTTPNotFound
+        next
+      end
       route && route.handler == "redirect"
     end
   end
