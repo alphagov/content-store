a20e9c5e0d09b874c48dd858f7ccaf8c7d3c0fc2
Tijmen Brommet <tijmen@gmail.com>
2016-11-29 14:15:13 +0000

    Run schema tests on CI
    Adds a jenkins task that will be run for PRs on govuk-content-schemas
    and clones the schemas before running the tests.

--- jenkins.sh
+++ jenkins.sh
@@ -11,5 +11,14 @@ bundle exec govuk-lint-ruby \
   --format clang \
   app config Gemfile lib script spec
 
+# Clone govuk-content-schemas depedency for contract tests
+rm -rf /tmp/govuk-content-schemas
+git clone git@github.com:alphagov/govuk-content-schemas.git /tmp/govuk-content-schemas
+(
+ cd /tmp/govuk-content-schemas
+ git checkout ${SCHEMA_GIT_COMMIT:-"deployed-to-production"}
+)
+export GOVUK_CONTENT_SCHEMAS_PATH=/tmp/govuk-content-schemas
+
 bundle exec rake db:mongoid:drop
 COVERAGE=on bundle exec rake ci:setup:rspec default
