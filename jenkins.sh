ae7eebe2eab39eeb2ebe09482e686e6680a8e617
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-12-09 17:54:46 +0000

    Add Jenkinsfile for running on Jenkins 2.
    We no longer need to call gh-status from jenkins.sh.

--- jenkins.sh
+++ jenkins.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-GH_STATUS_REPO_NAME=${REPO_NAME:-"alphagov/content-store"}
-CONTEXT_MESSAGE=${CONTEXT_MESSAGE:-"default"}
-GH_STATUS_GIT_COMMIT=${INITIATING_GIT_COMMIT:-${GIT_COMMIT}}
+set -e
 
 export RAILS_ENV=test
-
-function github_status {
-  STATUS="$1"
-  MESSAGE="$2"
-  gh-status "$GH_STATUS_REPO_NAME" "$GH_STATUS_GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" -c "$CONTEXT_MESSAGE" >/dev/null
-}
-
-function error_handler {
-  trap - ERR # disable error trap to avoid recursion
-  local parent_lineno="$1"
-  local message="$2"
-  local code="${3:-1}"
-  if [[ -n "$message" ]] ; then
-    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
-  else
-    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
-  fi
-  github_status error "errored on Jenkins"
-  exit "${code}"
-}
-
-trap 'error_handler ${LINENO}' ERR
-github_status pending "is running on Jenkins"
+export GOVUK_CONTENT_SCHEMAS_PATH=/tmp/govuk-content-schemas
+export COVERAGE=on
 
 # Cleanup anything left from previous test runs
 git clean -fdx
@@ -37,7 +14,6 @@ git clean -fdx
 # is master.
 git merge --no-commit origin/master || git merge --abort
 
-export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
 
 bundle exec govuk-lint-ruby \
@@ -52,15 +28,7 @@ git clone git@github.com:alphagov/govuk-content-schemas.git /tmp/govuk-content-s
  cd /tmp/govuk-content-schemas
  git checkout ${SCHEMA_GIT_COMMIT:-"deployed-to-production"}
 )
-export GOVUK_CONTENT_SCHEMAS_PATH=/tmp/govuk-content-schemas
 
 bundle exec rake db:mongoid:drop
 
-export COVERAGE=on
-
-if bundle exec rake ci:setup:rspec default; then
-  github_status success "succeeded on Jenkins"
-else
-  github_status failure "failed on Jenkins"
-  exit 1
-fi
+bundle exec rake ci:setup:rspec default
