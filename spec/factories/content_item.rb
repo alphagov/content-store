066e2dfafe3c8da0943f28147a5993276ae8b57f
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-12-09 13:07:26 +0000

    Allow fact check access via header
    If a valid JWT token is passed to authenticating-proxy it will extract
    the value to the Govuk-Fact-Check-Id header, which is passed downstream
    by gds-api-adapters.
    
    This change then checks that the header value matches a value in the
    fact_check_id array within the access_limited hash of the item being
    requested and returns it if so, otherwise sends a 403.
    
    Note that the presence of the header means the user has bypassed
    normal authentication, so we need to verify that they've specifically
    requested the item with that fact_check_id, and not fall through to
    the auth check.
    
    As with a normal access-limited item, we send a cache-control: private
    header when the request contains the content-id header.

--- spec/factories/content_item.rb
+++ spec/factories/content_item.rb
@@ -66,7 +66,10 @@ FactoryGirl.define do
     factory :access_limited_content_item, parent: :content_item do
       sequence(:base_path) { |n| "/access-limited-#{n}" }
       access_limited {
-        { "users" => ["M6GdNZggrbGiJrLjMSbKqA", "f17250b0-7540-0131-f036-005056030202"] }
+        {
+          "users" => ["M6GdNZggrbGiJrLjMSbKqA", "f17250b0-7540-0131-f036-005056030202"],
+          "fact_check_ids" => ["85aa9fd5-c514-4964-b931-5b597e4ec668"]
+        }
       }
     end
   end
