f826bc54ae8823a0bbde9acf024e0c30580bfe02
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2017-05-15 14:12:09 +0000

    Update configs for new version.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ require 'statsd-ruby'
 
 module ContentStore
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
