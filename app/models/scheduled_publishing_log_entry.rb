139212427a8e36d8aac3fb0c80700d51ab471dd1
Graham Pengelly <graham@three-tier.com>
2017-07-06 12:27:38 +0100

    Add ScheduledPublishingLogEntry
    This object will be persisted when a scheduled publication is saved to
    the content store and records base_path, document_type, expected
    publication time and actual publication time. It also saves the delay in
    milliseconds for convenience.
    
    This will replace the existing statsd logging which is ineffective due
    to the low volume of data.
    
    This will also allow us to identify individual items that were delayed
    should we need to retrieve this data upon request by publishers in the
    event of an issue with publication.

---
+++ app/models/scheduled_publishing_log_entry.rb
@@ -0,0 +1,18 @@
+class ScheduledPublishingLogEntry
+  include Mongoid::Document
+  include Mongoid::Timestamps::Created
+  field :base_path, type: String
+  field :document_type, type: String
+  field :scheduled_publication_time, type: DateTime
+  field :delay_in_milliseconds
+
+  before_save do |document|
+    document.delay_in_milliseconds = set_delay_in_milliseconds
+  end
+
+private
+
+  def set_delay_in_milliseconds
+    ((Time.now - scheduled_publication_time) * 1000.0).to_i
+  end
+end
---
+++ spec/models/scheduled_publishing_log_entry_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe ScheduledPublishingLogEntry do
+  describe "when created" do
+    it "sets the delay" do
+      publication_time = Time.now
+      allow(Time).to receive(:now)
+        .and_return(publication_time + 20)
+      log_entry = ScheduledPublishingLogEntry.create(
+        base_path: "/booyah",
+        document_type: "stats",
+        scheduled_publication_time: publication_time
+      )
+
+      expect(log_entry.delay_in_milliseconds).to be_within(1).of(20000)
+    end
+  end
+end
