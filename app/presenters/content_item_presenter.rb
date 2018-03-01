5aef7320b109cde06a24040d7a507d9b3e84e1e2
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2018-03-01 14:56:56 +0000

    Revert "Add scheduled publication time to content item response"
    This reverts commit cf1747115f9f00e3586c067fe014f6207ca91da5 (PR #391).
    
    This increased the response times by around 50% on staging, so revert
    this while we consider how to improve this, e.g. by adding an index to
    ScheduledPublishingLogEntry.

--- app/presenters/content_item_presenter.rb
+++ app/presenters/content_item_presenter.rb
@@ -33,10 +33,9 @@ class ContentItemPresenter
     publishing_request_id
   ).freeze
 
-  def initialize(item, api_url_method, scheduled_publishing: nil)
+  def initialize(item, api_url_method)
     @item = item
     @api_url_method = api_url_method
-    @scheduled_publishing = scheduled_publishing
   end
 
   def as_json(options = nil)
@@ -46,13 +45,12 @@ class ContentItemPresenter
       "details" => RESOLVER.resolve(item.details),
     ).tap do |i|
       i["redirects"] = item["redirects"] if i["schema_name"] == "redirect"
-      i["publishing_scheduled_at"] = scheduled_publishing.scheduled_publication_time if scheduled_publishing
     end
   end
 
 private
 
-  attr_reader :item, :api_url_method, :scheduled_publishing
+  attr_reader :item, :api_url_method
 
   def links
     ExpandedLinksPresenter.new(item.expanded_links).present
