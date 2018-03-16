7b43697be44f194eb7fdf6ecb4e75fc37136c869
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2018-03-16 14:30:13 +0000

    Revert "Add scheduled publication time to content item response"
    This reverts commit cf1747115f9f00e3586c067fe014f6207ca91da5.
    
    This feature will be reimplemented in a different way in the next
    commit, by adding the field to the content item at write time rather
    than at query time.

--- spec/factories/scheduled_publishing_log_entry.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :scheduled_publishing_log_entry, class: ScheduledPublishingLogEntry do
-    scheduled_publication_time Time.new(2018, 1, 1)
-  end
-end
--- spec/models/scheduled_publishing_log_entry_spec.rb
+++ spec/models/scheduled_publishing_log_entry_spec.rb
@@ -15,37 +15,4 @@ describe ScheduledPublishingLogEntry do
       expect(log_entry.delay_in_milliseconds).to be_within(1).of(20000)
     end
   end
-
-  describe "find latest by path" do
-    it "returns nil if there are no log entries for the given path" do
-      log_entry = ScheduledPublishingLogEntry.latest_with_path("/some_page")
-      expect(log_entry).to be_nil
-    end
-
-    it "returns a single log entry" do
-      expected_log_entry = ScheduledPublishingLogEntry.create(
-        base_path: "/a_scheduled_page",
-        scheduled_publication_time: Time.now,
-      )
-      log_entry = ScheduledPublishingLogEntry.latest_with_path("/a_scheduled_page")
-      expect(log_entry).to eq(expected_log_entry)
-    end
-
-    it "returns the log entry for the most recent publishing" do
-      ScheduledPublishingLogEntry.create(
-        base_path: "/some_path",
-        scheduled_publication_time: Time.new(2015, 5, 1),
-      )
-      ScheduledPublishingLogEntry.create(
-        base_path: "/some_path",
-        scheduled_publication_time: Time.new(2018, 3, 20),
-      )
-      ScheduledPublishingLogEntry.create(
-        base_path: "/some_path",
-        scheduled_publication_time: Time.new(2017, 12, 31),
-      )
-      log_entry = ScheduledPublishingLogEntry.latest_with_path("/some_path")
-      expect(log_entry.scheduled_publication_time).to eq(Time.new(2018, 3, 20))
-    end
-  end
 end
