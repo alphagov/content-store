cef7798592291c96bf550a5a2e6d37dec35886e6
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-01-03 11:03:12 +0000

    Fix hash indentation

--- spec/integration/end_to_end_spec.rb
+++ spec/integration/end_to_end_spec.rb
@@ -1,31 +1,32 @@
 require 'rails_helper'
 
 describe "End-to-end behaviour", type: :request do
-  let(:data) {
+  let(:data) do
     {
-    "locale" => "en",
-    "base_path" => "/vat-rates",
-    "content_id" => SecureRandom.uuid,
-    "title" => "VAT rates",
-    "format" => "answer",
-    "schema_name" => "answer",
-    "document_type" => "travel_advice",
-    "content_purpose_document_supertype" => "guidance",
-    "email_document_supertype" => "publications",
-    "government_document_supertype" => "new-stories",
-    "navigation_document_supertype" => "guidance",
-    "search_user_need_document_supertype" => "core",
-    "user_journey_document_supertype" => "finding",
-    "content_purpose_supergroup" => "guidance_and_regulation",
-    "content_purpose_subgroup" => "guidance",
-    "publishing_app" => "publisher",
-    "rendering_app" => "frontend",
-    "routes" => [
-      { "path" => "/vat-rates", "type" => 'exact' }
-    ],
-    "public_updated_at" => Time.now,
-    "payload_version" => "1",
-  }}
+      "locale" => "en",
+      "base_path" => "/vat-rates",
+      "content_id" => SecureRandom.uuid,
+      "title" => "VAT rates",
+      "format" => "answer",
+      "schema_name" => "answer",
+      "document_type" => "travel_advice",
+      "content_purpose_document_supertype" => "guidance",
+      "email_document_supertype" => "publications",
+      "government_document_supertype" => "new-stories",
+      "navigation_document_supertype" => "guidance",
+      "search_user_need_document_supertype" => "core",
+      "user_journey_document_supertype" => "finding",
+      "content_purpose_supergroup" => "guidance_and_regulation",
+      "content_purpose_subgroup" => "guidance",
+      "publishing_app" => "publisher",
+      "rendering_app" => "frontend",
+      "routes" => [
+        { "path" => "/vat-rates", "type" => 'exact' }
+      ],
+      "public_updated_at" => Time.now,
+      "payload_version" => "1",
+    }
+  end
 
   def create_item(data_hash)
     put_json "/content#{data_hash['base_path']}", data_hash
