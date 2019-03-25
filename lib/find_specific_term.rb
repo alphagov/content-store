a862af4800581a76daf45cb294dcc8a1f5858d7f
Keith Emmerson <keith.emmerson@digital.cabinet-office.gov.uk>
2019-03-25 11:42:20 +0000

    Include detailed guide description field in search
    The search of the content store doesn't seem to include the intro summary in detailed guides. Add this to the list of fields to search.

--- lib/find_specific_term.rb
+++ lib/find_specific_term.rb
@@ -52,6 +52,7 @@ private
 
   def content_items(term)
     ContentItem.or('details.body': term)
+      .or('description': term)
       .or('details.body.content': term)
       .or('details.parts.body': term)
       .or('details.parts.body.content': term)
