d5a1b65482be3f5bf844d5d21c406355c6e3cae5
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-07-06 12:03:44 +0100

    Remove db/migrations
    Following from @kevindew's suggestion, this removes the db migrations
    from content-store:
    https://github.com/alphagov/content-store/pull/744#pullrequestreview-442966860
    
    This is to discourage having 'data migrations' and instead have developers
    introduce temporary rake tasks for one-off tasks that they need to perform.

--- db/migrate/20160224143207_add_default_payload_version_to_content_items.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultPayloadVersionToContentItems < Mongoid::Migration
-  def self.up
-    ContentItem.where(payload_version: nil).update_all(payload_version: 0)
-  end
-end
--- db/migrate/20191011142024_remove_auth_by_pass_i_ds_from_access_limited.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAuthByPassIDsFromAccessLimited < Mongoid::Migration
-  def self.up
-    ContentItem
-      .where(:access_limited.nin => [{}, nil])
-      .each { |ci| ci.unset("access_limited.auth_bypass_ids") }
-  end
-
-  def self.down; end
-end
