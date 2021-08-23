2dda107a60373b82889fa4f19cc8196e198c587f
Michael Walker <mike@barrucadu.co.uk>
2021-08-23 14:40:32 +0100

    Run `app:update` task

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,17 @@
 require_relative "boot"
 
+require "rails"
 # Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
 # require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 # require "action_view/railtie"
+# require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -18,11 +25,15 @@ require "gds_api/router"
 module ContentStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = %i[
