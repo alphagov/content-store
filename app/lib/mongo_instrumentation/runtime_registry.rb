dd7bc13310c86000f334e56356a930978ba75544
Kevin Dew <kevindew@me.com>
2022-03-21 19:02:05 +0000

    Resolve PerThreadRegistry deprecation
    We can replace this with a `thread_mattr_accessor` variable (as per the
    Rails deprecation notice [1]). I've set instance_accessor to false since
    we only use this as a class method.
    
    [1]: https://github.com/rails/rails/blob/18ecdb12f7b0aa1f8f823fffb3ba190f65155602/activesupport/lib/active_support/per_thread_registry.rb#L43-L48

--- app/lib/mongo_instrumentation/runtime_registry.rb
+++ app/lib/mongo_instrumentation/runtime_registry.rb
@@ -3,12 +3,7 @@ require "active_support/per_thread_registry"
 module MongoInstrumentation
   # This is a namespaced thread locals registry for tracking the duration of
   # mongo queries.
-  #
-  # See the documentation of ActiveSupport::PerThreadRegistry for further
-  # details.
   class RuntimeRegistry
-    extend ActiveSupport::PerThreadRegistry
-
-    attr_accessor :mongo_runtime
+    thread_mattr_accessor :mongo_runtime, instance_accessor: false
   end
 end
