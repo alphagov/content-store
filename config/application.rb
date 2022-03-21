8ac68ce3970557e9d9da460a9181828e852aac37
Kevin Dew <kevindew@me.com>
2022-03-21 19:26:24 +0000

    Switch to Rails 7 framework defaults

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ require "gds_api/router"
 module ContentStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
