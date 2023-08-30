b3d40350e2e46b3b8261e1d67159a9c6470d4288
Al Davidson <apdavidson@gmail.com>
2023-08-30 10:46:02 +0100

    (re-)Allow null timestamps on records imported from mongo
    (re-)Allow null timestamps on records imported from mongo

---
+++ db/migrate/20230830093643_allow_null_timestamps.rb
@@ -0,0 +1,12 @@
+class AllowNullTimestamps < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :content_items, :created_at, true
+    change_column_null :content_items, :updated_at, true
+
+    change_column_null :publish_intents, :created_at, true
+    change_column_null :publish_intents, :updated_at, true
+
+    change_column_null :scheduled_publishing_log_entries, :created_at, true
+    change_column_null :scheduled_publishing_log_entries, :updated_at, true
+  end
+end
