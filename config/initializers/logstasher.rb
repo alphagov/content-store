a4f1d63c31da096e5d3bb0db3bc8c7535fca2186
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-11-23 16:35:11 +0000

    Fix logger custom field config
    It turns out that you can't call `LogStasher.add_custom_fields` twice.
    Because Content Store's custom fields config was being run after the
    govuk_app_config gem's own custom fields config, the gem's config was
    being overwritten. So, fields like `govuk_request_id` and `varnish_id`
    weren't appearing in Content Store's controller request logs (but
    `govuk_dependency_resolution_source_content_id` was).
    
    The gem (version 9.7.0) now provides a mechanism for setting custom
    fields that doesn't overwrite the gem's own settings
    https://docs.publishing.service.gov.uk/repos/govuk_app_config.html#logger-configuration:
    
    ```
    GovukJsonLogging.configure do
      add_custom_fields do |fields|
        fields[:govuk_custom_field] = request.headers["GOVUK-Custom-Header"]
      end
    end
    ```

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,7 +1,5 @@
-unless Rails.env.test?
-  GovukJsonLogging.configure do
-    add_custom_fields do |fields|
-      fields[:govuk_dependency_resolution_source_content_id] = request.headers["GOVUK-Dependency-Resolution-Source-Content-Id"]
-    end
+GovukJsonLogging.configure do
+  add_custom_fields do |fields|
+    fields[:govuk_dependency_resolution_source_content_id] = request.headers["GOVUK-Dependency-Resolution-Source-Content-Id"]
   end
 end
