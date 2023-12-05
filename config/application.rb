105005f812bddc4c40dbdfcc3dc86c6a7f1010eb
Al Davidson <apdavidson@gmail.com>
2023-12-05 12:50:47 +0000

    Fix 'undefined method minutes for Integer' on boot
    In between Rails 7.0.x and 7.1.x something changed in the way
    ActiveSupport extensions are loaded. This meant that in built
    containers, we were getting an error message along the lines of
    'undefined method minutes for 5:Integer' in config/application.rb
    
    Investigation showed that adding an explicit require of the
    relevant core extension fixed the problem

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_controller/railtie"
 # require "action_view/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
+require "active_support/core_ext/integer/time"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
