368b3099d224d0f5d292f6bb3bfc7849708746d7
Al Davidson <apdavidson@gmail.com>
2024-01-03 09:42:11 +0000

    Switch the /healthcheck/ready endpoint to check ActiveRecord
    The healthcheck endpoint was still checking Mongoid - this needs*
    switching to ActiveRecord.
    
    [Trello card](https://trello.com/c/dggI2ttX/988-remove-any-now-redundant-mongo-related-code-from-content-store)
    
    * Although, as this has been reporting a critical status for weeks
    but no-one noticed - is this endpoint even used anymore?

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
 
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
-    GovukHealthcheck::Mongoid,
+    GovukHealthcheck::ActiveRecord,
   )
 end
