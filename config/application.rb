48ad10eb64ea4b7636c111a021a45ed8ed6826d1
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-08-14 16:30:23 +0100

    Use Rails 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ require "gds_api/router"
 module ContentStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
