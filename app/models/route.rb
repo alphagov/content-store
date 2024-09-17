ea11a784f1f7c98a6d26ee698f93428c56a8db2e
Sean Rankine <sean.rankine@me.com>
2024-09-17 10:36:07 +0100

    Add find route logic

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,42 @@
 class Route < ApplicationRecord
-  belongs_to :content_item, required: false
-  belongs_to :publish_intent, required: false
+  belongs_to :content_item, optional: true
+  belongs_to :publish_intent, optional: true
+
+  def self.find_matching_route(path)
+    sql = <<-SQL
+    SELECT
+      routes.*,
+      content_items.rendering_app as content_item_rendering_app,
+      content_items.schema_name as content_item_schema_name,
+      content_items.details as content_item_details,
+      publish_intents.rendering_app as  publish_intent_rendering_app
+    FROM routes
+    LEFT JOIN content_items ON routes.content_item_id = content_items.id
+    LEFT JOIN publish_intents ON routes.publish_intent_id = publish_intents.id
+    WHERE
+      (routes.match_type = 'exact' AND routes.path = :path)
+      OR
+      (routes.match_type = 'prefix' AND :path LIKE routes.path || '%')
+    ORDER BY
+      CASE WHEN routes.match_type = 'exact' THEN 1 ELSE 2 END,  -- Prioritize exact matches first
+      LENGTH(routes.path) DESC
+    LIMIT 1
+    SQL
+
+    Route.find_by_sql([sql, { path: }]).first
+  end
+
+  def backend
+    if content_item
+      if content_item.gone?
+        "gone"
+      elsif content_item.redirect?
+        "redirect"
+      else
+        content_item.rendering_app
+      end
+    elsif publish_intent
+      publish_intent.rendering_app
+    end
+  end
 end
