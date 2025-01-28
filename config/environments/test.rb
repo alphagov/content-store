0876ef2a915f4db4fd55d0644cfb55a3ac75f52d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-01-28 12:47:04 +0000

    Run `rails app:update`
    This updates configuration for Rails 8.
    
    The following have been skipped:
    - puma configuration, since we use `GovukPuma`
    - enabling `force_ssl`, since this has previously caused issues in other
      applications

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,13 +17,11 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
+  # Configure public file server for tests with cache-control for performance.
+  config.public_file_server.headers = { "cache-control" => "public, max-age=3600" }
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
@@ -35,14 +33,14 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
